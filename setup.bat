61d23230318cc4b890a45453eb37beccd95782fe
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-07-06 17:06:50 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,6 @@
 @echo off
 
+rem Guidance for configuring IPv6 in Windows for advanced users
+rem https://support.microsoft.com/en-us/help/929852/guidance-for-configuring-ipv6-in-windows-for-advanced-users
+
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f
