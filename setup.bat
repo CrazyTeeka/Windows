bdc21a95f68d3f5383c8532e5013b02e186d59de
CrazyTeeka <47723943+CrazyTeeka@users.noreply.github.com>
2020-08-02 17:26:07 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,3 +4,4 @@ rem Guidance for configuring IPv6 in Windows for advanced users
 rem https://support.microsoft.com/en-us/help/929852/guidance-for-configuring-ipv6-in-windows-for-advanced-users
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 255 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f
